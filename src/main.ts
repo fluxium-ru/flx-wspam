2db38d44e86e25c8fad9b7dfef486ef3cf3299ee
fluxium-ru <hello@fluxium.ru>
2026-06-06 01:16:05 +1000

    +inquirer +title

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,21 @@
 // todo:
 // - add CLI for easier usage
 // - potentially package and deploy
+import inquirer from 'inquirer';
+
+
+const title = `
+ ██░ ██ ▓█████  ██▀███   ███▄ ▄███▓▓█████   ██████ 
+▓██░ ██▒▓█   ▀ ▓██ ▒ ██▒▓██▒▀█▀ ██▒▓█   ▀ ▒██    ▒ 
+▒██▀▀██░▒███   ▓██ ░▄█ ▒▓██    ▓██░▒███   ░ ▓██▄   
+░▓█ ░██ ▒▓█  ▄ ▒██▀▀█▄  ▒██    ▒██ ▒▓█  ▄   ▒   ██▒
+░▓█▒░██▓░▒████▒░██▓ ▒██▒▒██▒   ░██▒░▒████▒▒██████▒▒
+ ▒ ░░▒░▒░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░   ░  ░░░ ▒░ ░▒ ▒▓▒ ▒ ░
+ ▒ ░▒░ ░ ░ ░  ░  ░▒ ░ ▒░░  ░      ░ ░ ░  ░░ ░▒  ░ ░
+ ░  ░░ ░   ░     ░░   ░ ░      ░      ░   ░  ░  ░  
+ ░  ░  ░   ░  ░   ░            ░      ░  ░      ░  
+`
+
 
 type DiscordWebhookPayload = {
     content: string
@@ -13,26 +28,41 @@ const myPayload: DiscordWebhookPayload = { content: "CONTENT_HERE!" }
 class FlxWspam {
     private webhookURL: string = "REMOVED" // place webhook url here
 
-    public async SendPayload(): Promise<void> {
-        fetch(this.webhookURL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(myPayload)
-        })
-        .then(response => {
-            if (response.ok) {
-                console.log('Message sent successfully!');
-            } else {
-                console.error('Failed to send message:', response.statusText);
-            }
-        })
-        .catch(error => {
-            console.error('Error: ', error);
-        })
+    public async sendPayload(): Promise<void> {
+        try {
+            const response = await fetch(this.webhookURL, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(myPayload)
+                    });
+
+                    if (response.ok) {
+                        console.log('Message sent successfully!');
+                    } else {
+                        // server responded w error
+                        console.error('Error encountered, failed: ', response.status, response.statusText);
+                    }
+        } catch (error) {
+            // network or other errors
+            console.error('Error sending message: ', error);
+        }
     };
+
+    public async init() {
+        console.log(title);
+        const answers = await inquirer.prompt([
+            {
+                type: 'select',
+                name: 'optionMenu1',
+                message: 'Choose an option:',
+                choices: ['Set URL', 'Set Count', 'Set Content', 'Send']
+            }
+        ])
+        console.log(answers.optionMenu1);
+    }
 }
 
 const mySpam = new FlxWspam();
-mySpam.SendPayload();
+mySpam.init();
